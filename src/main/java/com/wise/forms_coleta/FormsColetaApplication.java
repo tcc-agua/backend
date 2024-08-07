c64deff581f3179860563da7349b9309f67c6d4f
ambercff <ambercf13@gmail.com>
2024-08-07 08:14:32 -0300

    :tada: first commit

---
+++ src/main/java/com/wise/forms_coleta/FormsColetaApplication.java
@@ -0,0 +1,13 @@
+package com.wise.forms_coleta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormsColetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormsColetaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wise/forms_coleta/FormsColetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wise.forms_coleta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormsColetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
