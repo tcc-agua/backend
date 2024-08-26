fdc1bbdec2183880f68384910b7d80227cd1ebce
ambercff <ambercf13@gmail.com>
2024-08-26 13:45:28 -0300

    :construction: excel

--- forms-coleta/src/main/java/com/wise/forms_coleta/implementations/exportar_excel/ExcelExportServiceImpl.java
+++ forms-coleta/src/main/java/com/wise/forms_coleta/implementations/exportar_excel/ExcelExportServiceImpl.java
@@ -16,9 +16,9 @@ import org.springframework.stereotype.Service;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class ExcelExportServiceImpl implements ExcelExportService {
@@ -35,6 +35,17 @@ public class ExcelExportServiceImpl implements ExcelExportService {
             List<Coleta> coletas = coletaRepository.findAll();
             List<Ponto> pontos = pontoRepository.findAll();
 
+            // Agrupando os dados por ponto
+            Map<Ponto, List<Coleta>> dadosPorPonto = pontos.stream()
+                    .collect(Collectors.toMap(
+                            ponto -> ponto,
+                            ponto -> coletas.stream()
+                                    .filter(coleta -> coleta.getPbSet().stream()
+                                            .anyMatch(pb -> pb.getPonto().equals(ponto))
+                                            || coleta.getCdSet().stream()
+                                            .anyMatch(cd -> cd.getPonto().equals(ponto)))
+                                    .collect(Collectors.toList())));
+
             // Criando uma aba única para todos os pontos
             Sheet sheet = workbook.createSheet("Dados Coleta");
             int rowNum = 0; // Começa na primeira linha
@@ -50,29 +61,26 @@ public class ExcelExportServiceImpl implements ExcelExportService {
             headerRow.createCell(cellIndex++).setCellValue("Hora Fim");
 
             // Adiciona os cabeçalhos dos pontos
-            Map<Ponto, Integer> pontoColumnMapping = new HashMap<>();
-            int totalColumns = 4; // Contador de colunas para os cabeçalhos gerais
             for (Ponto ponto : pontos) {
                 int numCols = ponto.getNome().contains("PB") ? 5 : 3;
-                pontoColumnMapping.put(ponto, totalColumns);
-                Cell pointHeaderCell = headerRow.createCell(totalColumns);
-                pointHeaderCell.setCellValue(ponto.getNome());
-                sheet.setColumnWidth(totalColumns, 6000); // Ajusta a largura da coluna
+                Cell pontoCell = headerRow.createCell(cellIndex);
+                pontoCell.setCellValue(ponto.getNome());
+                sheet.setColumnWidth(cellIndex, 6000); // Ajusta a largura da coluna
 
                 // Mescla as células do cabeçalho do ponto
-                sheet.addMergedRegion(new CellRangeAddress(0, 0, totalColumns, totalColumns + numCols - 1));
-                totalColumns += numCols; // Incrementa o índice da célula para o próximo ponto
+                sheet.addMergedRegion(new CellRangeAddress(0, 0, cellIndex, cellIndex + numCols - 1));
+                cellIndex += numCols; // Incrementa o índice da célula para o próximo ponto
             }
 
             // Adiciona a linha de subcabeçalhos para os dados dos pontos
             Row subHeaderRow = sheet.createRow(rowNum++);
-            int subHeaderIndex = 0;
+            cellIndex = 0;
 
             // Adiciona os subcabeçalhos gerais
-            subHeaderRow.createCell(subHeaderIndex++); // Espaço reservado para "Técnico"
-            subHeaderRow.createCell(subHeaderIndex++); // Espaço reservado para "Data"
-            subHeaderRow.createCell(subHeaderIndex++); // Espaço reservado para "Hora Início"
-            subHeaderRow.createCell(subHeaderIndex++); // Espaço reservado para "Hora Fim"
+            subHeaderRow.createCell(cellIndex++).setCellValue(""); // Espaço reservado para "Técnico"
+            subHeaderRow.createCell(cellIndex++).setCellValue(""); // Espaço reservado para "Data"
+            subHeaderRow.createCell(cellIndex++).setCellValue(""); // Espaço reservado para "Hora Início"
+            subHeaderRow.createCell(cellIndex++).setCellValue(""); // Espaço reservado para "Hora Fim"
 
             // Adiciona os subcabeçalhos dos pontos
             for (Ponto ponto : pontos) {
@@ -81,7 +89,8 @@ public class ExcelExportServiceImpl implements ExcelExportService {
                         new String[]{"Pressão (Kgf/cm²)", "Hidrômetro (m³)", "Rede Pluvial ou ETAS?"};
 
                 for (String header : headers) {
-                    subHeaderRow.createCell(subHeaderIndex++).setCellValue(header);
+                    Cell headerCell = subHeaderRow.createCell(cellIndex++);
+                    headerCell.setCellValue(header);
                 }
             }
 
@@ -93,49 +102,32 @@ public class ExcelExportServiceImpl implements ExcelExportService {
 
             // Adiciona os dados de coleta
             for (Coleta coleta : coletas) {
-                boolean hasData = false;
+                Row row = sheet.createRow(rowNum++);
+                cellIndex = 0;
 
-                // Verificação prévia dos dados antes de criar a linha
+                // Adiciona os dados principais da coleta
+                row.createCell(cellIndex++).setCellValue(coleta.getTecnico());
+                row.createCell(cellIndex++).setCellValue(coleta.getDataColeta().toString());
+                row.createCell(cellIndex++).setCellValue(coleta.getHora_inicio().toString());
+                row.createCell(cellIndex++).setCellValue(coleta.getHora_fim().toString());
+
+                // Adiciona os dados dos pontos
                 for (Ponto ponto : pontos) {
                     PBs pb = coleta.getPbSet().stream().filter(pbItem -> pbItem.getPonto().equals(ponto)).findFirst().orElse(null);
                     CD cd = coleta.getCdSet().stream().filter(cdItem -> cdItem.getPonto().equals(ponto)).findFirst().orElse(null);
 
-                    if ((ponto.getNome().contains("PB") && pb != null) || (ponto.getNome().contains("CD") && cd != null)) {
-                        hasData = true; // Há dados para este ponto
-                        break; // Se há dados, não precisamos continuar a verificação
-                    }
-                }
-
-                // Se houver dados, cria a linha e preenche
-                if (hasData) {
-                    Row row = sheet.createRow(rowNum++);
-                    cellIndex = 0;
-
-                    // Adiciona os dados principais da coleta
-                    row.createCell(cellIndex++).setCellValue(coleta.getTecnico());
-                    row.createCell(cellIndex++).setCellValue(coleta.getDataColeta().toString());
-                    row.createCell(cellIndex++).setCellValue(coleta.getHora_inicio().toString());
-                    row.createCell(cellIndex++).setCellValue(coleta.getHora_fim().toString());
-
-                    // Preenche os dados dos pontos
-                    for (Ponto ponto : pontos) {
-                        Integer startCol = pontoColumnMapping.get(ponto);
-                        if (startCol == null) continue;
-
-                        PBs pb = coleta.getPbSet().stream().filter(pbItem -> pbItem.getPonto().equals(ponto)).findFirst().orElse(null);
-                        CD cd = coleta.getCdSet().stream().filter(cdItem -> cdItem.getPonto().equals(ponto)).findFirst().orElse(null);
-
-                        if (ponto.getNome().contains("PB") && pb != null) {
-                            row.createCell(startCol).setCellValue(pb.getPressao() != null ? pb.getPressao().toString() : "");
-                            row.createCell(startCol + 1).setCellValue(pb.getPulsos() != null ? pb.getPulsos().toString() : "");
-                            row.createCell(startCol + 2).setCellValue(pb.getNivel_oleo() != null ? pb.getNivel_oleo().toString() : "");
-                            row.createCell(startCol + 3).setCellValue(pb.getNivel_agua() != null ? pb.getNivel_agua().toString() : "");
-                            row.createCell(startCol + 4).setCellValue(pb.getVol_rem_oleo() != null ? pb.getVol_rem_oleo().toString() : "");
-                        } else if (ponto.getNome().contains("CD") && cd != null) {
-                            row.createCell(startCol).setCellValue(cd.getPressao() != null ? cd.getPressao().toString() : "");
-                            row.createCell(startCol + 1).setCellValue(cd.getHidrometro() != null ? cd.getHidrometro().toString() : "");
-                            row.createCell(startCol + 2).setCellValue(cd.getTipo_rede() != null ? cd.getTipo_rede() : "");
-                        }
+                    if (ponto.getNome().contains("PB") && pb != null) {
+                        row.createCell(cellIndex++).setCellValue(pb.getPressao() != null ? pb.getPressao() : 0);
+                        row.createCell(cellIndex++).setCellValue(pb.getPulsos() != null ? pb.getPulsos() : 0);
+                        row.createCell(cellIndex++).setCellValue(pb.getNivel_oleo() != null ? pb.getNivel_oleo() : 0);
+                        row.createCell(cellIndex++).setCellValue(pb.getNivel_agua() != null ? pb.getNivel_agua() : 0);
+                        row.createCell(cellIndex++).setCellValue(pb.getVol_rem_oleo() != null ? pb.getVol_rem_oleo() : 0);
+                    } else if (ponto.getNome().contains("CD") && cd != null) {
+                        row.createCell(cellIndex++).setCellValue(cd.getPressao() != null ? cd.getPressao() : 0);
+                        row.createCell(cellIndex++).setCellValue(cd.getHidrometro() != null ? cd.getHidrometro() : 0);
+                        row.createCell(cellIndex++).setCellValue(cd.getTipo_rede() != null ? cd.getTipo_rede() : "Não especificado");
+                    } else {
+                        cellIndex += ponto.getNome().contains("PB") ? 5 : 3; // Avança o índice se não houver dados correspondentes
                     }
                 }
             }
@@ -158,13 +150,11 @@ public class ExcelExportServiceImpl implements ExcelExportService {
         return pointHeaderStyle;
     }
 
-    // Método para criar o estilo do subcabeçalho dos pontos
-    private CellStyle createSubHeaderStyle(XSSFWorkbook workbook) {
-        CellStyle subHeaderStyle = workbook.createCellStyle();
-        subHeaderStyle.setAlignment(HorizontalAlignment.CENTER);
-        subHeaderStyle.setVerticalAlignment(VerticalAlignment.CENTER);
-        subHeaderStyle.setFillForegroundColor(IndexedColors.LIGHT_CORNFLOWER_BLUE.getIndex());
-        subHeaderStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-        return subHeaderStyle;
+    // Método para criar o estilo dos cabeçalhos das colunas
+    private CellStyle createHeaderStyle(XSSFWorkbook workbook) {
+        CellStyle headerStyle = workbook.createCellStyle();
+        headerStyle.setAlignment(HorizontalAlignment.CENTER);
+        headerStyle.setVerticalAlignment(VerticalAlignment.CENTER);
+        return headerStyle;
     }
 }
