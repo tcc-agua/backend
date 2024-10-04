91dd6a3741d68ecebaa654568468b7bf6e68cf09
caiofariaas <caiofarias077@gmail.com>
2024-10-04 15:00:19 -0300

    :bug:fix: sensor-ph endpoint

--- forms-coleta/src/main/java/com/wise/forms_coleta/controllers/SensorPHController.java
+++ forms-coleta/src/main/java/com/wise/forms_coleta/controllers/SensorPHController.java
@@ -45,7 +45,7 @@ public class SensorPHController {
         return ResponseEntity.created(uri).body("Formulário preenchido com sucesso!");
     }
 
-    @GetMapping("/get-ph")
+    @GetMapping
     public ResponseEntity<List<SensorPHDTO>> getAll() {
         return new ResponseEntity<>(sensorPHGetAllService.getAll(), HttpStatus.OK);
     }
